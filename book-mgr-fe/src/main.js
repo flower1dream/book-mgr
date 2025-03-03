dd38a012d74536ca8a4a2a2f8fd55f103e317ba1
flowdream <949433954@qq.com>
2025-03-03 15:26:03 +0800

    完成登录和注册页面

--- book-mgr-fe/src/main.js
+++ book-mgr-fe/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import App from './App';
 import router from './router';
 import store from './store';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css'; // 3.x 版本的 CSS 文件
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).use(Antd).mount('#app');
---
+++ book-mgr-fe/src/views/Auth/index.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="auth">
+      <div class="bg"></div>
+      <div>
+        <img src="" alt="">
+        <h2 class="title">图书管理系统</h2>
+      </div>
+
+      <div class="form">
+        <a-tabs >
+          <a-tab-pane key="1" tab="登录">
+
+            <div class="item">
+            <a-input size="large" placeholder="账户">
+              <template #prefix>
+                <user-outlined />
+              </template>
+            </a-input>
+            
+            </div>
+
+            <div class="item">
+            <a-input size="large" placeholder="密码">
+              <template #prefix>
+                <lock-outlined />
+              </template>
+            </a-input>
+            </div>
+
+            <div class="item">
+              <a href="">忘记密码</a>
+            </div>
+
+            <div class="item">
+              <a-button size="large" type="primary">
+                登入
+              </a-button>
+            </div>
+
+          </a-tab-pane>
+          <a-tab-pane key="2" tab="注册">
+            <div class="item">
+            <a-input size="large" placeholder="账户">
+              <template #prefix>
+                <user-outlined />
+              </template>
+            </a-input>
+            </div>
+
+            <div class="item">
+            <a-input size="large" placeholder="密码">
+              <template #prefix>
+                <lock-outlined />
+              </template>              
+            </a-input>
+            </div>
+            
+            <div class="item">
+            <a-input size="large" placeholder="邀请码">
+              <template #prefix>
+                <mail-outlined />
+              </template>
+            </a-input>
+            </div>
+
+        
+            <div class="item">
+              <a-button size="large" type="primary">
+                注册
+              </a-button>
+            </div>
+          </a-tab-pane>
+          
+        </a-tabs>
+      </div>
+    </div>
+  </template>
+  
+  <script src="./index.js"></script>
+  
+  <style lang="scss" scoped>
+    @import './index.scss';
+  </style>
