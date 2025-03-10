a0f80fdee56d2bd9bfb46250f4db1986772015e9
flowdream <949433954@qq.com>
2025-03-10 19:39:51 +0800

    前端图书管理基础界面

--- book-mgr-fe/src/main.js
+++ book-mgr-fe/src/main.js
@@ -4,5 +4,7 @@ import router from './router';
 import store from './store';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css'; // 3.x 版本的 CSS 文件
+import SpaceBetween from './components/SpaceBetween/index.vue'
 
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+
+createApp(App).use(store).use(router).use(Antd).component('space-between',SpaceBetween).mount('#app');
